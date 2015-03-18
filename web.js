d46b9f1b5c06e3f0b19126f27e22466f0e04d2f6
xiang zhuyuan <xiangzhuyuan@gmail.com>
2015-03-18 23:02:56 +0800

    express dependancy

--- web.js
+++ web.js
@@ -1,4 +1,9 @@
+var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 3000);
+
+var logger = require('morgan');
+app.use(logger('dev')); //replaces your app.use(express.logger());
+
+app.use(gzippo.staticGzip("" + __dirname__ + "/dist"));
+app.listen(process.env.PORT || 5000);
