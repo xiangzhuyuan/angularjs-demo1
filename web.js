ce7d6074daa5e239e9d650349ffcbc1b8d416ef3
xiang zhuyuan <xiangzhuyuan@gmail.com>
2015-03-18 22:45:15 +0800

    fix express server

--- web.js
+++ web.js
@@ -1,7 +1,4 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname__ + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 3000);
